ee9ec46888856bcbad5a4c34399c06ce3a97da5d
Alessandro Campagni <campagni@extendi.it>
2017-02-14 12:03:13 +0100

    fix wrong merge

--- lib/alchemy-api/combined_call.rb
+++ lib/alchemy-api/combined_call.rb
@@ -1,30 +1,12 @@
 module AlchemyAPI
   class CombinedCall < Base
-    Config.add_mode :combined, self
-
-    # Parameter :extract
-    # Comma separated list of requested features. Possible values:
-    # page-image
-    # image-kw
-    # feed
-    # entity
-    # keyword
-    # taxonomy
-    # concept
-    # relation
-    # pub-date
-    # doc-sentiment
+    Config.add_mode :combined_call, self
 
     def web_method
       "#{method_prefix}GetCombinedData"
     end
 
     private
-
-    def check_options(opts)
-      super(opts)
-      fail MissingOptionsError unless options[:extract]
-    end
 
     def indexer
       nil
